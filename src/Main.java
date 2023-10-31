16bfa65e31276ed1dc1403b24160ed4ba70e2d4d
mesh-dell <mwaimonicas@gmail.com>
2023-10-31 19:26:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+class bankAccount
+{
+    int acNo;
+    String acName, acPhone, acEmail;
+    double AcBal;
+    public bankAccount(int acNo, String acName, String acPhone, String acEmail, double acBal)
+    {
+        this.acNo = acNo;
+        this.acName = acName;
+        this.acPhone = acPhone;
+        this.acEmail = acEmail;
+        this.AcBal = acBal;
+    }
+    public void accountHolder()
+    {
+        System.out.println("Account Number :" + acNo);
+        System.out.println("Account Holder Name :" + acName);
+        System.out.println("Account Holder Address :" + acPhone);
+        System.out.println("Account Holder Phone :" + acEmail);
+        System.out.println("Account Holder Balance :" + AcBal);
+    }
+    public void deposit(double ammount)
+    {
+        AcBal += ammount;
+        System.out.println("Deposited: $" + ammount);
+    }
+    public void displayBalance()
+    {
+        System.out.println("Account Number: " + acNo + "\tBalance: $" + AcBal);
+    }
+}
+
+class SavingsAccount extends bankAccount
+{
+    public SavingsAccount(int acNo, String acName, String acPhone, String acEmail, double acBal)
+    {
+        super(acNo, acName, acPhone, acEmail, acBal);
+    }
+}
+
+class CurrentAccount extends bankAccount
+{
+    public CurrentAccount(int acNo, String acName, String acPhone, String acEmail, double acBal)
+    {
+        super(acNo, acName, acPhone, acEmail, acBal);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SAVINGS ACCOUNT DETAILS");
+        SavingsAccount sac = new SavingsAccount(1001, "Ann Kamau", "07200000", "test@gmail.com", 1000);
+        sac.accountHolder();
+        sac.deposit(500);
+        sac.displayBalance();
+        CurrentAccount cac = new CurrentAccount(1001, "Peter Smith", "08100000", "admin@gmail.com", 0.0);
+        System.out.println("\nCURRENT ACCOUNT DETAILS");
+        cac.accountHolder();
+        cac.deposit(300);
+        cac.displayBalance();
+    }
+}
